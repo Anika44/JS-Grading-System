c9f09cfc299a8d64dba53cd1b850719f8085a48f
Anika44 <70100163+Anika44@users.noreply.github.com>
2020-09-28 01:40:43 -0700

    Add files via upload

---
+++ math.js
@@ -0,0 +1,11 @@
+<script>
+var myName="Anika";
+console.log("My name is",myName);
+
+var myAge="22";
+console.log("My age is",myAge);
+
+const pi ="3.141";
+console.log("value of pi is",pi);
+
+</script>
